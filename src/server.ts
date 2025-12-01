378c5dc835208155e79b8dbb784fef105748de00
hildean dantas <hildeandantas@gmail.com>
2025-12-01 02:23:53 -0300

    add: port config

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,7 @@ import WebSocket from "./lib/socket";
 const PORT = process.env.PORT || 3000;
 const app = new App().app;
 
-const server = app.listen(PORT || 3000, () => {
+const server = app.listen(Number(PORT), '0.0.0.0', () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
 
