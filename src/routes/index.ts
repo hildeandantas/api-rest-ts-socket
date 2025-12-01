5298062e59f2f8b6ba36573e8909a9bd628f82f6
hildean dantas <hildeandantas@gmail.com>
2025-12-01 02:42:08 -0300

    add: ambiente utilizado

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,8 +3,11 @@ import express from "express";
 const indexRoutes = () => {
   const router = express.Router();
 
+  const environment = process.env.NODE_ENV || 'local';
+
+
   router.get("/", (req, res) => {
-    res.status(200).json({ message: "Hello World!" });
+    res.status(200).json({ message: "Hello World!", ambiente: environment });
   });
 
   return router;
--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,19 @@ import "dotenv/config";
 import App from "./app";
 import WebSocket from "./lib/socket";
 
-const PORT = process.env.PORT || 3000;
 const app = new App().app;
+const PORT = process.env.PORT || 3000;
+const environment = process.env.NODE_ENV || 'local';
+
+if (environment === 'production') {
+  console.log('🔒 Log de segurança ativado (Produção)');
+} else {
+  console.log('🐛 Modo Debug ativado (Dev/Homolog)');
+}
 
 const server = app.listen(Number(PORT), '0.0.0.0', () => {
   console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Ambiente sendo utilizado: ${environment}`)
 });
 
 new WebSocket(server);
